6750e16dd5ac2b3692639dd97965258fb0944df5
danwardvs <danwardvs@adsgames.net>
2014-09-08 18:45:00 -0700

    Shootable asteroids, slow as poop

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include<allegro.h>
 #include<alpng.h>
 #include<cmath>
+#include<time.h>
 
 BITMAP* buffer;
 BITMAP* cursor;
 BITMAP* spaceship;
 BITMAP* gun;
+BITMAP* asteroid_large;
 
 float distance_to_mouse;
 float angle_radians;
@@ -25,6 +27,15 @@ bool debugmode;
 bool create_bullet;
 int bullet_delay;
 
+struct asteroid{
+    bool on_screen;
+    float x;
+    float y;
+    float vector_x;
+    float vector_y;
+    int size;
+}asteroids[100];
+
 struct bullet{
     float x;
     float y;
@@ -32,6 +43,24 @@ struct bullet{
     float vector_y;
     bool on_screen=false;
 }bullets[100];
+
+//A function for collision, used for mouse clicking
+bool collision(int xMin1, int xMax1, int xMin2, int xMax2, int yMin1, int yMax1, int yMin2, int yMax2)
+{
+  if (xMin1 < xMax2 && yMin1 < yMax2 && xMin2 < xMax1 && yMin2 < yMax1){
+    return true;
+  }
+  return false;
+}
+
+//Random number function
+int random(int newLowest, int newHighest)
+{
+  int lowest = newLowest, highest = newHighest;
+  int range = (highest - lowest) + 1;
+  int randomNumber = lowest+int(range*rand()/(RAND_MAX + 1.0));
+  return randomNumber;
+}
 
 //A function that handles error messages
 void abort_on_error(const char *message){
@@ -58,25 +87,8 @@ float find_angle(int x_1, int y_1, int x_2, int y_2){
 
     return atan2(tan_1,tan_2);
 }
-
-
-void update(){
-
-    //Calls the function, solves the math
-    distance_to_mouse=distance_to_object(mouse_x,mouse_y,player_x,player_y);
-    angle_radians=find_angle(player_x,player_y,mouse_x,mouse_y);
-
-    //Converts radians to usable(by allegro) values
-    angle_degrees=(angle_radians*57.2957795);
-    angle_allegro=(angle_degrees/1.41176470588);
-
-    angle_x=sin(angle_degrees);
-    angle_y=cos(angle_degrees);
-
-    //Moves the point
-
-
-    //Draws the screen
+void draw(){
+ //Draws the screen
     rectfill(buffer,0,0,800,600,makecol(10,0,0));
     if(key[KEY_F1])debugmode=true;
     if(key[KEY_F2])debugmode=false;
@@ -92,6 +104,12 @@ void update(){
     draw_sprite(buffer,spaceship,player_x-23,player_y-19);
     rotate_sprite(buffer,gun,player_x,player_y,itofix(angle_allegro));
 
+
+    for(int i=0; i<100; i++){
+        if(asteroids[i].on_screen){
+            draw_sprite(buffer,asteroid_large,asteroids[i].x,asteroids[i].y);
+        }
+    }
     for(int i=0; i<100; i++){
         if(bullets[i].on_screen){
             putpixel(buffer,bullets[i].x,bullets[i].y,makecol(255,0,0));
@@ -99,6 +117,27 @@ void update(){
     }
     draw_sprite(buffer,cursor,mouse_x-10,mouse_y-10);
     draw_sprite(screen,buffer,0,0);
+
+
+}
+
+void update(){
+    draw();
+    //Calls the function, solves the math
+    distance_to_mouse=distance_to_object(mouse_x,mouse_y,player_x,player_y);
+    angle_radians=find_angle(player_x,player_y,mouse_x,mouse_y);
+
+    //Converts radians to usable(by allegro) values
+    angle_degrees=(angle_radians*57.2957795);
+    angle_allegro=(angle_degrees/1.41176470588);
+
+    angle_x=sin(angle_degrees);
+    angle_y=cos(angle_degrees);
+
+    //Moves the point
+
+
+
 
     bullet_delay++;
     if(key[KEY_SPACE] && bullet_delay>9 || mouse_b & 1 && bullet_delay>9 ){
@@ -115,23 +154,45 @@ void update(){
     //    player_y+=vector_y;
     //}
 
+    //Asteroids code
+     for(int i=0; i<100; i++){
+        if(asteroids[i].on_screen){
+           asteroids[i].x+=asteroids[i].vector_x;
+           asteroids[i].y+=asteroids[i].vector_y;
+
+
+           if(asteroids[i].x>1000 || asteroids[i].y>800 || asteroids[i].x<-200 || asteroids[i].y<-200)asteroids[i].on_screen=false;
+        }else if(random(1,100)==1){
+            asteroids[i].on_screen=true;
+           asteroids[i].x=random(-200,1000);
+           asteroids[i].y=random(-200,800);
+           asteroids[i].vector_x=-2*cos(random(-3,3));
+           asteroids[i].vector_y=-2*sin(random(-3,3));
+
+
+        }
+    }
 
     for(int i=0; i<100; i++){
         if(bullets[i].on_screen){
            bullets[i].x+=bullets[i].vector_x;
            bullets[i].y+=bullets[i].vector_y;
-
-
+            //Check collision
+            for(int j=0; j<100; j++){
+                if(collision(bullets[i].x,bullets[i].x+100,asteroids[j].x,asteroids[j].x,bullets[i].y,bullets[i].y+100,asteroids[j].y,asteroids[j].y))asteroids[j].on_screen=false;
+            }
            if(bullets[i].x>800 || bullets[i].y>600 || bullets[i].x<0 || bullets[i].y<0)bullets[i].on_screen=false;
         }else if(create_bullet==true){
             bullets[i].on_screen=true;
             create_bullet=false;
-            bullets[i].x=player_x;
-            bullets[i].y=player_y;
             bullets[i].vector_x=-2*cos(angle_radians);
             bullets[i].vector_y=-2*sin(angle_radians);
+            bullets[i].x=player_x;
+            bullets[i].y=player_y;
+
         }
     }
+
     rest(2);
 }
 
@@ -154,6 +215,9 @@ void setup(){
     if(!(gun = load_bitmap("gun.png",NULL))){
         abort_on_error( "Cannot find gun.png.\n Please check your files and try again.");
     }
+    if(!(asteroid_large = load_bitmap("asteroid_large.png",NULL))){
+        abort_on_error( "Cannot find asteroid_large.png.\n Please check your files and try again.");
+    }
 }
 
 
@@ -163,6 +227,7 @@ void setup(){
 
 int main(){
 
+  srand(time(NULL));
   allegro_init();
   alpng_init();
   install_timer();
@@ -171,7 +236,7 @@ int main(){
   set_color_depth(32);
 
   set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800,600, 0, 0);
-
+  install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,".");
 
 
   set_window_title("Angle Finder");
