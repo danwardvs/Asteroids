5b365a9fa0d746fa9b18e50bb8a7b99319a35ee7
danwardvs <danwardvs@adsgames.net>
2014-09-08 21:22:11 -0700

    Added asteroid sizes, shoot noises, content++;

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ BITMAP* cursor;
 BITMAP* spaceship;
 BITMAP* gun;
 BITMAP* asteroid_large;
+BITMAP* asteroid_medium;
+BITMAP* asteroid_small;
+BITMAP* asteroid_smaller;
+
+SAMPLE* fire;
+SAMPLE* asteroid_shot;
 
 float distance_to_mouse;
 float angle_radians;
@@ -26,6 +32,15 @@ bool debugmode;
 
 bool create_bullet;
 int bullet_delay;
+
+struct particle{
+    int x;
+    int y;
+    int r;
+    int g;
+    int b;
+    bool on_screen;
+}particles[1000];
 
 struct asteroid{
     bool on_screen;
@@ -87,6 +102,37 @@ float find_angle(int x_1, int y_1, int x_2, int y_2){
 
     return atan2(tan_1,tan_2);
 }
+void create_particles(int x, int y, int amount){
+    for(int i=0; i<amount;){
+        for(int j=0; j<1000; j++){
+            if(!particles[j].on_screen){
+                particles[j].on_screen=true;
+                particles[j].x=x;
+                particles[j].y=y;
+                int randnum=random(1,3);
+                if(randnum==1){
+                    particles[j].r=51;
+                    particles[j].g=51;
+                    particles[j].b=51;
+                }else if(randnum==2){
+                    particles[j].r=145;
+                    particles[j].g=29;
+                    particles[j].b=29;
+                }else if(randnum==2){
+                    particles[j].r=84;
+                    particles[j].g=16;
+                    particles[j].b=16;
+                }
+
+                i++;
+
+
+            }
+        }
+    }
+
+}
+
 void draw(){
  //Draws the screen
     rectfill(buffer,0,0,800,600,makecol(10,0,0));
@@ -107,7 +153,10 @@ void draw(){
 
     for(int i=0; i<100; i++){
         if(asteroids[i].on_screen){
-            draw_sprite(buffer,asteroid_large,asteroids[i].x,asteroids[i].y);
+            if(asteroids[i].size==4)draw_sprite(buffer,asteroid_large,asteroids[i].x,asteroids[i].y);
+            if(asteroids[i].size==3)draw_sprite(buffer,asteroid_medium,asteroids[i].x,asteroids[i].y);
+            if(asteroids[i].size==2)draw_sprite(buffer,asteroid_small,asteroids[i].x,asteroids[i].y);
+            if(asteroids[i].size==1)draw_sprite(buffer,asteroid_smaller,asteroids[i].x,asteroids[i].y);
         }
     }
     for(int i=0; i<100; i++){
@@ -115,6 +164,13 @@ void draw(){
             putpixel(buffer,bullets[i].x,bullets[i].y,makecol(255,0,0));
         }
     }
+    for(int i=0; i<1000; i++){
+        if(particles[i].on_screen){
+            putpixel(buffer,particles[i].x,particles[i].y,makecol(particles[i].r,particles[i].g,particles[i].b));
+        }
+
+    }
+
     draw_sprite(buffer,cursor,mouse_x-10,mouse_y-10);
     draw_sprite(screen,buffer,0,0);
 
@@ -140,9 +196,10 @@ void update(){
 
 
     bullet_delay++;
-    if(key[KEY_SPACE] && bullet_delay>9 || mouse_b & 1 && bullet_delay>9 ){
+    if(key[KEY_SPACE] && bullet_delay>99 || mouse_b & 1 && bullet_delay>99 ){
         create_bullet=true;
         bullet_delay=0;
+        play_sample(fire,125,125,1000,0);
     }
 
 
@@ -154,6 +211,18 @@ void update(){
     //    player_y+=vector_y;
     //}
 
+
+    //Particles
+    for(int i=0; i<1000; i++){
+        if(particles[i].on_screen){
+            if(particles[i].x>800 || particles[i].x<0 || particles[i].y>600 || particles[i].y<0)particles[i].on_screen=false;
+            particles[i].x+=random(-2,2);
+            particles[i].y+=random(-2,2);
+            if(random(1,100)==1)particles[i].on_screen=false;
+        }
+    }
+
+
     //Asteroids code
      for(int i=0; i<100; i++){
         if(asteroids[i].on_screen){
@@ -162,12 +231,13 @@ void update(){
 
 
            if(asteroids[i].x>1000 || asteroids[i].y>800 || asteroids[i].x<-200 || asteroids[i].y<-200)asteroids[i].on_screen=false;
-        }else if(random(1,100)==1){
-            asteroids[i].on_screen=true;
+        }else if(random(1,10000)==1){
+           asteroids[i].size=4;
+           asteroids[i].on_screen=true;
            asteroids[i].x=random(-200,1000);
-           asteroids[i].y=random(-200,800);
-           asteroids[i].vector_x=-2*cos(random(-3,3));
-           asteroids[i].vector_y=-2*sin(random(-3,3));
+           asteroids[i].y=-100;
+           asteroids[i].vector_x=(-2*cos(random(-3,3)))/4;
+           asteroids[i].vector_y=(-2*sin(random(-3,3)))/4;
 
 
         }
@@ -179,21 +249,40 @@ void update(){
            bullets[i].y+=bullets[i].vector_y;
             //Check collision
             for(int j=0; j<100; j++){
-                if(collision(bullets[i].x,bullets[i].x+100,asteroids[j].x,asteroids[j].x,bullets[i].y,bullets[i].y+100,asteroids[j].y,asteroids[j].y))asteroids[j].on_screen=false;
+              if(asteroids[j].on_screen){
+
+                bool hit;
+                hit=false;
+                if(asteroids[j].size==1 && collision(asteroids[j].x,asteroids[j].x+70,bullets[i].x,bullets[i].x,asteroids[j].y+30,asteroids[j].y+70,bullets[i].y,bullets[i].y))hit=true;
+                if(asteroids[j].size==2 && collision(asteroids[j].x,asteroids[j].x+80,bullets[i].x,bullets[i].x,asteroids[j].y+20,asteroids[j].y+80,bullets[i].y,bullets[i].y))hit=true;
+                if(asteroids[j].size==3 && collision(asteroids[j].x,asteroids[j].x+90,bullets[i].x,bullets[i].x,asteroids[j].y+10,asteroids[j].y+90,bullets[i].y,bullets[i].y))hit=true;
+                if(asteroids[j].size==4 && collision(asteroids[j].x,asteroids[j].x+100,bullets[i].x,bullets[i].x,asteroids[j].y,asteroids[j].y+100,bullets[i].y,bullets[i].y))hit=true;
+
+                if(hit){
+                    asteroids[j].size--;
+                    if(asteroids[j].size<1)asteroids[j].on_screen=false;
+                    bullets[i].on_screen=false;
+                    play_sample(asteroid_shot,125,125,1000,0);
+                    create_particles(asteroids[j].x+50,asteroids[j].y+50,10);
+
+
+
+                    }
+                }
             }
            if(bullets[i].x>800 || bullets[i].y>600 || bullets[i].x<0 || bullets[i].y<0)bullets[i].on_screen=false;
         }else if(create_bullet==true){
             bullets[i].on_screen=true;
             create_bullet=false;
-            bullets[i].vector_x=-2*cos(angle_radians);
-            bullets[i].vector_y=-2*sin(angle_radians);
+            bullets[i].vector_x=-3*cos(angle_radians);
+            bullets[i].vector_y=-3*sin(angle_radians);
             bullets[i].x=player_x;
             bullets[i].y=player_y;
 
         }
     }
 
-    rest(2);
+    rest(1);
 }
 
 
@@ -218,6 +307,24 @@ void setup(){
     if(!(asteroid_large = load_bitmap("asteroid_large.png",NULL))){
         abort_on_error( "Cannot find asteroid_large.png.\n Please check your files and try again.");
     }
+    if(!(asteroid_medium = load_bitmap("asteroid_medium.png",NULL))){
+        abort_on_error( "Cannot find asteroid_medium.png.\n Please check your files and try again.");
+    }
+    if(!(asteroid_small = load_bitmap("asteroid_small.png",NULL))){
+        abort_on_error( "Cannot find asteroid_small.png.\n Please check your files and try again.");
+    }
+    if(!(asteroid_smaller = load_bitmap("asteroid_smaller.png",NULL))){
+        abort_on_error( "Cannot find asteroid_smaller.png.\n Please check your files and try again.");
+    }
+
+    if (!(fire = load_sample("fire.wav"))){
+        abort_on_error("Cannot find sample fire.wav\nPlease check your files and try again");
+    }
+    if (!(asteroid_shot = load_sample("asteroid_shot.wav"))){
+        abort_on_error("Cannot find sample asteroid_shot.wav\nPlease check your files and try again");
+    }
+
+
 }
 
 
